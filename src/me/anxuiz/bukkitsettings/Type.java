afd9edf9f6f2d5f654fa6aee5bde47f0c7ee689c
Steve Anton <anxuiz.nx@gmail.com>
2012-10-17 16:38:51 -0700

    Add the serialize method to Type.

--- src/me/anxuiz/bukkitsettings/Type.java
+++ src/me/anxuiz/bukkitsettings/Type.java
@@ -4,7 +4,12 @@ import java.text.ParseException;
 
 public interface Type {
     String getName();
+
     boolean isInstance(Object obj);
-    Object parse(String raw) throws ParseException;
+
     String print(Object obj) throws IllegalArgumentException;
+
+    String serialize(Object obj) throws IllegalArgumentException;
+
+    Object parse(String raw) throws ParseException;
 }
