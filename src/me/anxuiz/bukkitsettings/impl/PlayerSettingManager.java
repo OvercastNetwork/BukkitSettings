20df21061f839d13c02ac6e3f16f8eb8b371167b
Steve Anton <anxuiz.nx@gmail.com>
2012-10-17 18:14:43 -0700

    Implement PlayerSettingManager.

---
+++ src/me/anxuiz/bukkitsettings/impl/PlayerSettingManager.java
@@ -0,0 +1,68 @@
+package me.anxuiz.bukkitsettings.impl;
+
+import java.util.List;
+
+import me.anxuiz.bukkitsettings.Setting;
+
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.plugin.Plugin;
+
+import com.google.common.base.Preconditions;
+
+public class PlayerSettingManager extends AbstractSettingManager {
+    protected final Plugin parent;
+    protected final Player player;
+
+    public PlayerSettingManager(Plugin parent, Player player) {
+        Preconditions.checkNotNull(parent);
+        Preconditions.checkNotNull(player);
+
+        this.parent = parent;
+        this.player = player;
+    }
+
+    @Override
+    public Object getValue(Setting setting) {
+        Preconditions.checkNotNull(setting);
+
+        Object value = this.getMetadataValue(getMetadataKey(setting));
+
+        if(value != null && setting.getType().isInstance(value)) {
+            return value;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void setValue(Setting setting, Object value) {
+        Preconditions.checkNotNull(setting);
+        Preconditions.checkNotNull(value);
+        Preconditions.checkArgument(setting.getType().isInstance(value));
+
+        this.player.setMetadata(getMetadataKey(setting), new FixedMetadataValue(this.parent, value));
+    }
+
+    @Override
+    public void deleteValue(Setting setting) {
+        Preconditions.checkNotNull(setting);
+
+        this.player.removeMetadata(getMetadataKey(setting), this.parent);
+    }
+
+    private static String getMetadataKey(Setting setting) {
+        return "setting." + setting.getName().toLowerCase();
+    }
+
+    private Object getMetadataValue(String key) {
+        List<MetadataValue> values = this.player.getMetadata(key);
+        for(MetadataValue value : values) {
+            if(value.getOwningPlugin() == this.parent) {
+                return value.value();
+            }
+        }
+        return null;
+    }
+}
