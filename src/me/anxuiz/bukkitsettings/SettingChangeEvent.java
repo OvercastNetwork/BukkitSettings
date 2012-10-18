7ba4fb585a3235a2c5b500cf5e3c0a047c753670
Steve Anton <anxuiz.nx@gmail.com>
2012-10-17 18:18:15 -0700

    Allow SettingChangeEvent to have null old / new values.

--- src/me/anxuiz/bukkitsettings/SettingChangeEvent.java
+++ src/me/anxuiz/bukkitsettings/SettingChangeEvent.java
@@ -1,28 +1,30 @@
 package me.anxuiz.bukkitsettings;
 
+import javax.annotation.Nullable;
+
 import me.anxuiz.bukkitsettings.util.TypeUtil;
 
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 
 public class SettingChangeEvent extends Event implements Cancellable {
     private static HandlerList handlers = new HandlerList();
 
     protected final Setting setting;
-    protected final Object oldValue;
-    protected final Object newValue;
+    protected final @Nullable Object oldValue;
+    protected final @Nullable Object newValue;
 
     protected boolean cancelled = false;
 
-    public SettingChangeEvent(Setting setting, Object oldValue, Object newValue) {
+    public SettingChangeEvent(Setting setting, @Nullable Object oldValue, @Nullable Object newValue) {
         Preconditions.checkNotNull(setting);
-        Preconditions.checkNotNull(oldValue);
-        Preconditions.checkArgument(setting.getType().isInstance(oldValue));
-        Preconditions.checkNotNull(newValue);
-        Preconditions.checkArgument(setting.getType().isInstance(newValue));
+        Preconditions.checkArgument(Objects.equal(oldValue, newValue), "old value may not equal new value");
+        if(oldValue != null) Preconditions.checkArgument(setting.getType().isInstance(oldValue));
+        if(newValue != null) Preconditions.checkArgument(setting.getType().isInstance(newValue));
 
         this.setting = setting;
         this.oldValue = oldValue;
@@ -33,11 +35,11 @@ public class SettingChangeEvent extends Event implements Cancellable {
         return this.setting;
     }
 
-    public <T> T getOldValue(Class<T> typeClass) throws IllegalArgumentException {
+    public @Nullable <T> T getOldValue(Class<T> typeClass) throws IllegalArgumentException {
         return TypeUtil.getValue(this.oldValue, typeClass);
     }
 
-    public <T> T getNewValue(Class<T> typeClass) throws IllegalArgumentException {
+    public @Nullable <T> T getNewValue(Class<T> typeClass) throws IllegalArgumentException {
         return TypeUtil.getValue(this.newValue, typeClass);
     }
 
--- src/me/anxuiz/bukkitsettings/util/TypeUtil.java
+++ src/me/anxuiz/bukkitsettings/util/TypeUtil.java
@@ -5,8 +5,13 @@ import com.google.common.base.Preconditions;
 public class TypeUtil {
     @SuppressWarnings("unchecked")
     public static <T> T getValue(Object value, Class<T> typeClass) throws IllegalArgumentException {
-        Preconditions.checkArgument(value.getClass().isAssignableFrom(typeClass), "value may not be cast to %s" + typeClass.getName());
-        return (T) value;
+        Preconditions.checkNotNull(typeClass);
+        if(value != null) {
+            Preconditions.checkArgument(value.getClass().isAssignableFrom(typeClass), "value may not be cast to %s" + typeClass.getName());
+            return (T) value;
+        } else {
+            return null;
+        }
     }
 
     private TypeUtil() {
