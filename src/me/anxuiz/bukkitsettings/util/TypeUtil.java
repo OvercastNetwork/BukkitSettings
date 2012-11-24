781bd4343a2613ce4aa1f92023cbbf64ef09a4ca
Steve Anton <anxuiz.nx@gmail.com>
2012-11-24 10:47:24 -0800

    Add @Nullable annotations to TypeUtil

--- src/me/anxuiz/bukkitsettings/util/TypeUtil.java
+++ src/me/anxuiz/bukkitsettings/util/TypeUtil.java
@@ -1,13 +1,15 @@
 package me.anxuiz.bukkitsettings.util;
 
+import javax.annotation.Nullable;
+
 import com.google.common.base.Preconditions;
 
 public class TypeUtil {
     @SuppressWarnings("unchecked")
-    public static <T> T getValue(Object value, Class<T> typeClass) throws IllegalArgumentException {
+    public static @Nullable <T> T getValue(@Nullable Object value, Class<T> typeClass) throws IllegalArgumentException {
         Preconditions.checkNotNull(typeClass);
         if(value != null) {
-            Preconditions.checkArgument(value.getClass().isAssignableFrom(typeClass), "value may not be cast to %s" + typeClass.getName());
+            Preconditions.checkArgument(value.getClass().isAssignableFrom(typeClass), "value may not be cast to %s", typeClass.getName());
             return (T) value;
         } else {
             return null;
