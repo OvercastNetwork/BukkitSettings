b9bd8ccc5c7a5fb98d2f6e771f254118aec2efa2
Steve Anton <anxuiz.nx@gmail.com>
2012-11-25 12:12:48 -0800

    Implement a draft of the setting command.

---
+++ src/main/java/me/anxuiz/settings/bukkit/plugin/Commands.java
@@ -0,0 +1,8 @@
+package me.anxuiz.settings.bukkit.plugin;
+
+import org.bukkit.ChatColor;
+
+public class Commands {
+    public static final String PLAYERS_ONLY = ChatColor.RED + "Settings are only available to players.";
+    public static final String SETTING_NOT_FOUND = ChatColor.RED + "No settings matched query.";
+}
--- src/main/java/me/anxuiz/settings/bukkit/plugin/SettingCommand.java
+++ src/main/java/me/anxuiz/settings/bukkit/plugin/SettingCommand.java
@@ -1,12 +1,46 @@
 package me.anxuiz.settings.bukkit.plugin;
 
+import me.anxuiz.settings.Setting;
+import me.anxuiz.settings.bukkit.PlayerSettings;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.google.common.base.Splitter;
 
 public class SettingCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // TODO
-        return false;
+        if(args.length < 1) {
+            return false;
+        }
+
+        Setting setting = PlayerSettings.getRegistry().get(args[0], true);
+
+        if(setting != null) {
+            this.sendSettingInfo(sender, setting);
+        } else {
+            sender.sendMessage(Commands.SETTING_NOT_FOUND);
+        }
+
+        return true;
+    }
+
+    public void sendSettingInfo(CommandSender sender, Setting setting) {
+        sender.sendMessage(ChatColor.GOLD + setting.getName());
+        sender.sendMessage(ChatColor.RED + "Type: " + setting.getType().getName());
+        sender.sendMessage(ChatColor.RED + setting.getSummary());
+        if(setting.hasDescription()) {
+            for(String line : Splitter.on('\n').split(setting.getDescription())) {
+                sender.sendMessage(ChatColor.GREEN + line);
+            }
+        }
+        sender.sendMessage("Default: " + setting.getType().print(setting.getDefaultValue()));
+        if(sender instanceof Player) {
+            Player player = (Player) sender;
+            sender.sendMessage("Current value: " + setting.getType().print(PlayerSettings.getManager(player).getValue(setting)));
+        }
     }
 }
