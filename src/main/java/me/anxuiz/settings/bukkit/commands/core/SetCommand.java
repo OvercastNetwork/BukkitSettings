9672362a3c34acfddc2fa740b1f64d666ecf1bcb
Taylor Blau <me@ttaylorr.com>
2014-01-18 23:00:45 -0500

    Convert /set to command framework

--- src/main/java/me/anxuiz/settings/bukkit/commands/core/SetCommand.java
+++ src/main/java/me/anxuiz/settings/bukkit/commands/core/SetCommand.java
@@ -1,7 +1,5 @@
 package me.anxuiz.settings.bukkit.commands.core;
 
-import java.util.Arrays;
-
 import me.anxuiz.settings.Setting;
 import me.anxuiz.settings.SettingManager;
 import me.anxuiz.settings.TypeParseException;
@@ -10,50 +8,49 @@ import me.anxuiz.settings.bukkit.commands.util.Commands;
 import me.anxuiz.settings.bukkit.commands.util.Permissions;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.google.common.base.Joiner;
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandException;
 
-public class SetCommand implements CommandExecutor {
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+public class SetCommand {
+
+    @Command(
+        aliases = {"set"},
+        usage = "<setting>, <value>",
+        desc = "Gets the option you have selected for a specific setting",
+        min = 2
+    )
+    public static void set(CommandContext args, CommandSender sender) throws CommandException {
         if(!(sender instanceof Player)) {
-            sender.sendMessage(Commands.PLAYERS_ONLY);
-            return true;
-        }
-
-        if(args.length < 2) {
-            return false;
+            throw new CommandException(Commands.PLAYERS_ONLY);
         }
 
         Player player = (Player) sender;
 
-        Setting setting = PlayerSettings.getRegistry().get(args[0], true);
+        Setting setting = PlayerSettings.getRegistry().get(args.getString(0), true);
 
         if(setting != null && Permissions.hasViewPermission(sender, setting)) {
             if(Permissions.hasSetPermission(sender, setting)) {
                 // try to parse
-                String raw = Joiner.on(' ').join(Arrays.asList(args).subList(1, args.length));
+                String raw = args.getJoinedStrings(1);
                 Object value;
                 try {
                     value = setting.getType().parse(raw);
                 } catch (TypeParseException e) {
-                    sender.sendMessage(ChatColor.RED + "Failed to parse: " + raw + " (type: " + setting.getType().getName() + ")");
-                    sender.sendMessage(ChatColor.RED + "Error: " + e.getMessage());
-                    return true;
+                    throw new CommandException("Failed to parse: " + raw + " (type: " + setting.getType().getName() + ")" + "\n" +
+                                                ChatColor.RED + "Error: " + e.getMessage());
                 }
                 SettingManager manager = PlayerSettings.getManager(player);
                 manager.setValue(setting, value);
                 Commands.sendSettingValue(sender, manager, setting);
             } else {
-                sender.sendMessage(Commands.NO_PERMISSION);
+                throw new CommandException(Commands.NO_PERMISSION);
             }
         } else {
-            sender.sendMessage(Commands.SETTING_NOT_FOUND);
+            throw new CommandException(Commands.SETTING_NOT_FOUND);
         }
-
-        return true;
-    }
+   }
 }
