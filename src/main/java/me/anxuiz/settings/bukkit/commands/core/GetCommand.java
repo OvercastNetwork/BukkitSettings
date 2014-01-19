ef26e8b3bf2c7352e5decd8c8f9ccc289ca6ae0c
Taylor Blau <me@ttaylorr.com>
2014-01-18 22:55:48 -0500

    Convert /get to use the command framework

--- src/main/java/me/anxuiz/settings/bukkit/commands/core/GetCommand.java
+++ src/main/java/me/anxuiz/settings/bukkit/commands/core/GetCommand.java
@@ -5,33 +5,36 @@ import me.anxuiz.settings.bukkit.PlayerSettings;
 import me.anxuiz.settings.bukkit.commands.util.Commands;
 import me.anxuiz.settings.bukkit.commands.util.Permissions;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class GetCommand implements CommandExecutor {
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandException;
+
+public class GetCommand {
+    
+    @Command(
+        aliases = {"get"},
+        usage = "<setting>",
+        desc = "Gets the option you have selected for a specific setting",
+        min = 1,
+        max = 1
+    )
+    public static void get(CommandContext args, CommandSender sender) throws CommandException {
         if(!(sender instanceof Player)) {
-            sender.sendMessage(Commands.PLAYERS_ONLY);
-            return true;
+            throw new CommandException(Commands.PLAYERS_ONLY);
         }
 
-        if(args.length < 1) {
-            return false;
-        }
-
-        Setting setting = PlayerSettings.getRegistry().get(args[0], true);
+        Setting setting = PlayerSettings.getRegistry().get(args.getString(0), true);
         if(setting != null && Permissions.hasViewPermission(sender, setting)) {
             if(Permissions.hasGetPermission(sender, setting)) {
                 Commands.sendSettingValue(sender, setting);
             } else {
-                sender.sendMessage(Commands.NO_PERMISSION);
+                throw new CommandException(Commands.NO_PERMISSION);
             }
         } else {
-            sender.sendMessage(Commands.SETTING_NOT_FOUND);
+            throw new CommandException(Commands.SETTING_NOT_FOUND);
         }
-
-        return true;
     }
 }
