0480880b44cc54c3169cc0a53129b2694547569c
Taylor Blau <me@ttaylorr.com>
2014-01-18 22:46:27 -0500

    Register commands with the framework
    This commit breaks all commands

--- src/main/java/me/anxuiz/settings/bukkit/plugin/BukkitSettingsPlugin.java
+++ src/main/java/me/anxuiz/settings/bukkit/plugin/BukkitSettingsPlugin.java
@@ -3,8 +3,20 @@ package me.anxuiz.settings.bukkit.plugin;
 import javax.annotation.Nullable;
 
 import me.anxuiz.settings.Setting;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sk89q.bukkit.util.CommandsManagerRegistration;
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.minecraft.util.commands.CommandPermissionsException;
+import com.sk89q.minecraft.util.commands.CommandUsageException;
+import com.sk89q.minecraft.util.commands.CommandsManager;
+import com.sk89q.minecraft.util.commands.MissingNestedCommandException;
+import com.sk89q.minecraft.util.commands.WrappedCommandException;
 
 public final class BukkitSettingsPlugin extends JavaPlugin {
 
@@ -13,6 +25,8 @@ public final class BukkitSettingsPlugin extends JavaPlugin {
         return inst;
     }
 
+    private CommandsManager<CommandSender> commands;
+    
     @Override
     public void onDisable() {
         inst = null;
@@ -21,13 +35,6 @@ public final class BukkitSettingsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         inst = this;
-
-        // Assign Executors
-        this.getCommand("get").setExecutor(new GetCommand());
-        this.getCommand("set").setExecutor(new SetCommand());
-        this.getCommand("setting").setExecutor(new SettingCommand());
-        this.getCommand("settings").setExecutor(new SettingsCommand());
-        this.getCommand("toggle").setExecutor(new ToggleCommand());
 
         // Assign Tab Completers
         this.getCommand("get").setTabCompleter(new GenericTabCompleter(
@@ -54,4 +61,45 @@ public final class BukkitSettingsPlugin extends JavaPlugin {
                 }
         ));
     }
+    
+    private void setupCommands() {
+        this.commands = new CommandsManager<CommandSender>() {
+            @Override
+            public boolean hasPermission(CommandSender sender, String perm) {
+                return sender.hasPermission(perm) | sender instanceof ConsoleCommandSender;
+            }
+        };
+        
+        CommandsManagerRegistration register = new CommandsManagerRegistration(this, this.commands);
+        register.register(GetCommand.class);
+        register.register(SetCommand.class);
+        register.register(SettingCommand.class);
+        register.register(SettingsCommand.class);
+        register.register(ToggleCommand.class);
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        try {
+            this.commands.execute(cmd.getName(), args, sender, sender);
+        } catch (CommandPermissionsException e) {
+            sender.sendMessage(ChatColor.RED + "You don't have permission.");
+        } catch (MissingNestedCommandException e) {
+            sender.sendMessage(ChatColor.RED + e.getUsage());
+        } catch (CommandUsageException e) {
+            sender.sendMessage(ChatColor.RED + e.getMessage());
+            sender.sendMessage(ChatColor.RED + e.getUsage());
+        } catch (WrappedCommandException e) {
+            if (e.getCause() instanceof NumberFormatException) {
+                sender.sendMessage(ChatColor.RED + "Number expected, string received instead.");
+            } else {
+                sender.sendMessage(ChatColor.RED + "An error has occurred. See console.");
+                e.printStackTrace();
+            }
+        } catch (CommandException e) {
+            sender.sendMessage(ChatColor.RED + e.getMessage());
+        }
+
+        return true;
+    }    
 }
