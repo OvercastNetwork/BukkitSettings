7a24b11c2f291231fc3df756aead88bfc2ed8782
Taylor Blau <me@ttaylorr.com>
2014-01-18 23:05:56 -0500

    Convert /setting to command framework

--- src/main/java/me/anxuiz/settings/bukkit/commands/core/SettingCommand.java
+++ src/main/java/me/anxuiz/settings/bukkit/commands/core/SettingCommand.java
@@ -1,48 +1,56 @@
 package me.anxuiz.settings.bukkit.commands.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.anxuiz.settings.Setting;
 import me.anxuiz.settings.bukkit.PlayerSettings;
 import me.anxuiz.settings.bukkit.commands.util.Commands;
 import me.anxuiz.settings.bukkit.commands.util.Permissions;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.google.common.base.Splitter;
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandException;
 
-public class SettingCommand implements CommandExecutor {
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length < 1) {
-            return false;
-        }
+public class SettingCommand {
 
-        Setting setting = PlayerSettings.getRegistry().get(args[0], true);
+    @Command(
+        aliases = {"get"},
+        usage = "<setting>",
+        desc = "Gets the option you have selected for a specific setting",
+        max = 1
+    )
+    public static void setting(CommandContext args, CommandSender sender) throws CommandException {
+        Setting setting = PlayerSettings.getRegistry().get(args.getString(0), true);
 
         if(setting != null && Permissions.hasViewPermission(sender, setting)) {
-            this.sendSettingInfo(sender, setting);
+            sender.sendMessage((String[]) getSettingInfo(sender, setting).toArray());
         } else {
-            sender.sendMessage(Commands.SETTING_NOT_FOUND);
+            throw new CommandException(Commands.SETTING_NOT_FOUND);
         }
-
-        return true;
     }
 
-    public void sendSettingInfo(CommandSender sender, Setting setting) {
-        sender.sendMessage(Commands.formatHeader(ChatColor.YELLOW + setting.getName()));
-        sender.sendMessage(ChatColor.YELLOW + "Summary: " + ChatColor.RESET + setting.getSummary());
+    public static List<String> getSettingInfo(CommandSender sender, Setting setting) {
+        List<String> info = new ArrayList<String>();
+        info.add(Commands.formatHeader(ChatColor.YELLOW + setting.getName()));
+        info.add(ChatColor.YELLOW + "Summary: " + ChatColor.RESET + setting.getSummary());
         if(setting.hasDescription()) {
             for(String line : Splitter.on('\n').split(setting.getDescription())) {
-                sender.sendMessage(ChatColor.YELLOW + line);
+                info.add(ChatColor.YELLOW + line);
             }
         }
-        sender.sendMessage(ChatColor.YELLOW + "Type: " + ChatColor.RESET + setting.getType().getName());
-        sender.sendMessage(ChatColor.YELLOW + "Default value: " + ChatColor.RESET + setting.getType().print(setting.getDefaultValue()));
+        info.add(ChatColor.YELLOW + "Type: " + ChatColor.RESET + setting.getType().getName());
+        info.add(ChatColor.YELLOW + "Default value: " + ChatColor.RESET + setting.getType().print(setting.getDefaultValue()));
         if(sender instanceof Player) {
             Object value = PlayerSettings.getManager((Player) sender).getValue(setting);
-            sender.sendMessage(ChatColor.YELLOW + "Current value: " + ChatColor.RESET + setting.getType().print(value));
+            info.add(ChatColor.YELLOW + "Current value: " + ChatColor.RESET + setting.getType().print(value));
         }
+
+        return info;
     }
 }
