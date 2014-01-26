99e4e363b796671d2a96f1aceeef4547be68944b
Jedediah Smith <jedediah@silencegreys.com>
2014-01-26 04:32:35 -0500

    Support optional settings

--- src/main/java/me/anxuiz/settings/bukkit/PlayerSettingCallback.java
+++ src/main/java/me/anxuiz/settings/bukkit/PlayerSettingCallback.java
@@ -1,6 +1,7 @@
 package me.anxuiz.settings.bukkit;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import me.anxuiz.settings.Setting;
 import me.anxuiz.settings.SettingCallback;
@@ -9,9 +10,9 @@ import me.anxuiz.settings.SettingManager;
 import org.bukkit.entity.Player;
 
 public abstract class PlayerSettingCallback implements SettingCallback {
-    public abstract void notifyChange(@Nonnull Player player, @Nonnull Setting setting, @Nonnull Object oldValue, @Nonnull Object newValue);
+    public abstract void notifyChange(@Nonnull Player player, @Nonnull Setting setting, @Nullable Object oldValue, @Nullable Object newValue);
 
-    public void notifyChange(@Nonnull SettingManager manager, @Nonnull Setting setting, @Nonnull Object oldValue, @Nonnull Object newValue) {
+    public void notifyChange(@Nonnull SettingManager manager, @Nonnull Setting setting, @Nullable Object oldValue, @Nullable Object newValue) {
         if(manager instanceof PlayerSettingManager) {
             Player player = ((PlayerSettingManager) manager).getPlayer();
             this.notifyChange(player, setting, oldValue, newValue);
